6366992b24eb5cbbf578a75ff5284467d6acc06c
kpmlegorka <77540800+kpmlegorka@users.noreply.github.com>
2021-01-21 18:26:49 +0300

    Update app.py

--- app.py
+++ app.py
@@ -17,33 +17,19 @@ df=read_data()
 X = df[['Kq','angle/90', 'h/lo', 'D/lo', 'd/lo', 'u/lo', 's/lo', 'Pr']]
 y = df['a/a_smooth_Bor']
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=0)
-
+y_forest=0.123456
 @st.cache
-def rndFors():
+if rndFors:
     rndm=RandomForestRegressor(n_estimators=100, max_features ='sqrt')
-
     rndm.fit(X_train, y_train)
-    rndm_y_pred = rndm.predict(X)
-    firstY=df['a/a_smooth_Bor']
-    firstY=firstY.values
-    endYYY=rndm_y_pred/firstY
-    endYYY=pd.DataFrame(data=endYYY)
-    endYYY=endYYY.loc[:,0]
-    return endYYY
+    data_slider = {'Kq': [x1], 'угол/90': [x2], 'h/lo': [x3], 'D/lo': [x4], 'd/lo': [x5], 'u/lo': [x6], 's/lo': [x7]}
+    nm = pd.DataFrame(data=data_slider)
+    y_forest=rndm.predict(nm)
+    
 
-YYY=rndFors()
-plt.scatter(df['q'], YYY, edgecolor='b')
-plt.ylim([0.1, 10])
-plt.xlim([1000, 10000000])
-plt.yscale('log')
-plt.xscale('log')
-plt.hlines(1, 0, 10000000, color = 'black', linewidth = 1)
-plt.hlines(3, 0, 10000000, color = 'black', linewidth = 2)
-plt.hlines(0.6, 0, 10000000, color = 'black', linewidth = 2)
-plt.text(1500, 3.2, '+30%')
-plt.text(1500, 0.5, '-30%')
-#plt.show()
-st.pyplot(plt)
+
+
+
 
 
 
@@ -71,7 +57,9 @@ if genre == '3D':
     x7 = st.sidebar.slider('s/lo', min_value=0.01, max_value=1.00)
 
     y=1.49*x1**(-0.15)*x2**(-1.720)*x3**(0.313)*x4**(0.069)*x5**(0.078)*x6**(-0.454)*x7**(-0.492)
-
+    
+    
+    
     col1, col2= st.beta_columns(2)
     with col1:
         st.header("3D структура")
@@ -79,7 +67,9 @@ if genre == '3D':
     with col2:
         st.header("Значение теплоотдачи")  
         st.write('Kq=', x1,'; ','угол/90=', x2,'; ','h/lo=', x3,'; ','D/lo=', x4,'; ','d/lo=', x5,'; ','u/lo=', x6,'; ','s/lo=', x7)
-        st.write('α/α0=',y)
+        st.write('Формула: α/α0=',y)
+        st.checkbox("show forest", False)
+        st.write('Лес: α/α0=',y_forest)
 else:
 #Kq
     x1 = st.sidebar.slider('Kq', min_value=2, max_value=12000)

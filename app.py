008cc86830b789aa234f7a6f1e8f5d6c421818d1
kpmlegorka <77540800+kpmlegorka@users.noreply.github.com>
2021-01-21 18:39:30 +0300

    Update app.py

--- app.py
+++ app.py
@@ -19,8 +19,8 @@ y = df['a/a_smooth_Bor']
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=0)
 y_forest=0.123456
 
-st.checkbox("show forest", False)
-
+rndFors=st.checkbox("show forest", False)
+@st.cache
 if rndFors:
     rndm=RandomForestRegressor(n_estimators=100, max_features ='sqrt')
     rndm.fit(X_train, y_train)

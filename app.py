34820814a2b691c74530f316345a363d765495f4
kpmlegorka <77540800+kpmlegorka@users.noreply.github.com>
2021-04-11 13:54:52 +0300

    Update app.py

--- app.py
+++ app.py
@@ -14,6 +14,9 @@ rndm = joblib.load('rndmF_model.pkl')
 Xmodel=joblib.load('XGBR_model.pkl')
 lm=joblib.load('GBR_model.pkl')
 
+rndm2 = joblib.load('rndmF_model_2.pkl')
+Xmodel2=joblib.load('XGBR_model_2.pkl')
+lm2=joblib.load('GBR_model_2.pkl')
 
 
 colum1, colum2, colum3= st.beta_columns(3)
@@ -26,7 +29,7 @@ with colum3:
 
 
 
-genre = st.radio("Выберите вид структуры 3D или 2D",('3D', '2D'))
+genre = st.radio("Выберите вид структуры: 3D или 2D",('3D', '2D'))
 if genre == '3D':
 #Kq
     x1 = st.sidebar.slider('Kq', min_value=8, max_value=12000,  value=1190)
@@ -96,11 +99,12 @@ else:
 
 #d/lo
     x5 = st.sidebar.slider('δ/lo', min_value=0.01, max_value=1.00,  value=0.22)
-
+#Pr
+    x6 = st.sidebar.slider('Pr', min_value=1.7, max_value=6.8,  value=1.75)
 
     y=2.66*x1**(-0.09)*x2**(-0.091)*x3**(0.133)*x4**(0.035)*x5**(-0.149)
     
-    data_slider = {'Kq': [x1], 'угол/90': [x2], 'h/lo': [x3], 'D/lo': [x4], 'd/lo': [x5]}
+    data_slider = {'Kq': [x1], 'угол/90': [x2], 'h/lo': [x3], 'D/lo': [x4], 'd/lo': [x5], 'Pr': [x6]}
     nm = pd.DataFrame(data=data_slider)
     xnm=np.array([[x1, x2, x3, x4, x5]])
     col1, col2= st.beta_columns(2)
@@ -109,17 +113,17 @@ else:
         st.image('2d.jpg',  use_column_width=True)
     with col2:
         st.header("Значение теплоотдачи")
-        st.write('Kq=', x1,'; ','угол/90=', x2,'; ','h/lo=', x3,'; ','Δ/lo=', x4,'; ','δ/lo=', x5)
+        st.write('Kq=', x1,'; ','угол/90=', x2,'; ','h/lo=', x3,'; ','Δ/lo=', x4,'; ','δ/lo=', x5,'; ','Pr=', x6)
         st.write('Формула: α/α0=',round(y, 2))
         if rndFors:
             #rndm=RandomForestRegressor(n_estimators=100, max_features ='sqrt')
             #rndm.fit(XU_train, yU_train)
-            y_forest=rndm.predict(nm)
+            y_forest=rndm2.predict(nm)
             st.write('Лес: α/α0=',round(y_forest[0], 2))
         if linReg:
             #lm = LinearRegression()
             #model = lm.fit(XU_train, yU_train)
-            y_linReg = lm.predict(nm)
+            y_linReg = lm2.predict(nm)
             st.write('ЛинРегрессия: α/α0=',round(y_linReg[0], 2))
         if nerKa:
             #dataset=xdd.to_numpy()      
@@ -127,5 +131,5 @@ else:
             #y_np = dataset[:,7]
             #Xmodel = xgboost.XGBRegressor()
             #Xmodel.fit(X_np, y_np)
-            y_nerKa = Xmodel.predict(xnm)
+            y_nerKa = Xmodel2.predict(xnm)
             st.write('Градиент: α/α0=',round(y_nerKa[0], 2))
